f0a88c35f8ee7dacdec8c03cbadea727378f97b2
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-01-10 17:36:11 +0000

    Rename power.py to power1.py

---
+++ power1.py
@@ -0,0 +1,15 @@
+def isPowerOfTwo(n): 
+    if (n == 0): 
+        return False
+    while (n != 1): 
+            if (n % 2 != 0): 
+                return False
+            n = n // 2
+              
+    return True
+  
+n1=int(input()) 
+if(isPowerOfTwo(n1)): 
+    print('yes') 
+else: 
+    print('no') 
