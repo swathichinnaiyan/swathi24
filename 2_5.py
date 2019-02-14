27bb82dfc1eff6aa43e9f14de4a8398af56c40e9
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-02-14 15:48:19 +0530

    Create 2_5.py

---
+++ 2_5.py
@@ -0,0 +1,6 @@
+r=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+s=input()
+for i in range(0,21):
+	if s==r[i]:
+		print(i+1)
+		break
