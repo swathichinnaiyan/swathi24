72d33247507160cd30ba19ff439518f1e9abdbef
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-02-16 10:08:18 +0000

    Update 83.py

--- 83.py
+++ 83.py
@@ -1,13 +1,6 @@
-#swa
-s=input()
-o=['/','%']
-for i in s:
-	if i in o:
-		if(i=='/'):
-			a=int(s.split(i)[0])
-			b=int(s.split(i)[1])
-			print(a//b)
-		else:
-			a=int(s.split(i)[0])
-			b=int(s.split(i)[0])
-			print(a%b)
+n,s,m=map(str,input().split())
+if s=="/":
+	print(int(n)//int(m))
+else:
+	print(int(n)%int(m))
+  #n
