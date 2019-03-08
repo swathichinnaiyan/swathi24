38ebff130166121decca7c4e40380c21f13a3eb7
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-03-08 16:07:23 +0000

    Update hun-1.py

--- hun-1.py
+++ hun-1.py
@@ -1,11 +1,20 @@
 #swa
 n=int(input())
 l=list(map(int,input().split()))
-l1=[]
-for i in range(0,len(l)):
-	for j in range(i+1,len(l)):
-		if l[i]==l[j]:
-			l1.append(l[i])
-for i in range(0,len(l1)-1):
-	print(l1[i],end=" ")
-print(l1[-1])	
+r=[]
+f=""
+for i in l:
+    if(l.count(i)>1):
+        r.append(i)
+if(len(r)==0):
+    print("unique")
+else:
+    s=set(r)
+    s=list(s)
+    s=sorted(s)
+    for i in s:
+        f=f+str(i)+" "
+    print(f.strip())
+
+    
+       
