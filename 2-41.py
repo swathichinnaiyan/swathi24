d2e661597228c59d6be5879c7c1cbd51caba7f01
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-03-09 16:25:58 +0000

    Update 2-41.py

--- 2-41.py
+++ 2-41.py
@@ -1,7 +1,13 @@
 #swa
-import math
-n,k=map(int,input().split(" "))
-if math.pow(n,k):
+n,k=map(int,input().split())
+for i in range(0,1000):
+	if k**i==n:
+		flag=0
+		break
+	else:
+		flag=1
+if flag==0:
 	print("yes")
 else:
 	print("no")
+© 2019 GitHub, Inc.
