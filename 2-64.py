776f08883a4249e195b55c6752793dd21ef38e9b
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-03-11 09:36:01 +0000

    Update 2-64.py

--- 2-64.py
+++ 2-64.py
@@ -1,12 +1,11 @@
 #swa
-n,k=map(int,input().split(" "))
+n,k=map(int,input().split())
+l=[int(x) for x in input().split()]
+l.sort()
 l1=[]
-l2=[]
-l=list(map(int,input().split(" ")))
 for i in range(0,len(l)):
-	if l[i]<k:
-		l1.append(l[i])
-l2=sorted(l1)
-for i in range(0,len(l2)-1):
-	print(l2[i],end=' ')
-print(l2[-1])	
+     if l[i]<k:
+        l1.append(l[i])
+for i in range(0,len(l1)-1):
+     print(l1[i],end=" ")
+print(l1[-1])
