3bb71a0d640b8c7fb6793ce32c0c8ee7f31c886b
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-03-11 09:24:26 +0000

    Update 2-63.py

--- 2-63.py
+++ 2-63.py
@@ -1,14 +1,13 @@
 #swa
 n=int(input())
-l=[]
-a=list(map(int,input().split(" ")))
-b=list(map(int,input().split(" ")))
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+j=0
+s=""
 for i in range(0,len(a)):
-	for j in range(0,len(b)):
-		if a[i]==b[j]:
-			l.append(a[i])
-			continue
-for i in range(0,len(l)-1):
-	print(l[i],end=" ")
-print(l[-1])	
-		
+    for j in range(0,len(b)):
+        if a[i]==b[j]:
+            s=s+str(a[i])+" "
+            b.remove(b[j])
+            break
+print(s.strip())
