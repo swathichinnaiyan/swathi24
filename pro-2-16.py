f2d3a7d3d90a28b7969029373b533a0d0ea20491
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-03-11 10:15:48 +0000

    Update pro-2-16.py

--- pro-2-16.py
+++ pro-2-16.py
@@ -1,12 +1,14 @@
 #swa
 n=int(input())
-l=list(map(int,input().split()))
-k=list(set(l))
-k.sort()
-c=0
-j=1
-for i in range(0,len(k)):
-    p=l.count(k[i])
-    c=c+p*j
-    j=j+1
-print(c)
+k=list(map(int,input().split()))
+s=[]
+s=[1]*n
+for i in range(n):
+    if i==0:
+        if k[i]>k[i+1]:
+            s[i]=s[i]+s[i+1]
+    elif i>0:
+        if k[i]>k[i-1]:
+            s[i]=s[i]+s[i-1]
+print(sum(s))
+           
