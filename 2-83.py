b3fe99e9f6200f0a4ee09b97cacfccbc71b323dd
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-03-16 12:31:38 +0000

    Update 2-83.py

--- 2-83.py
+++ 2-83.py
@@ -1,7 +1,7 @@
 #swa
 n=int(input())
-l=list(map(int,input().split(" ")))
+l=list(map(int,input().split()))
 s=l[0]
-for i in range(0,len(l)):
-	c=s| l[i]
-print(c)	
+for i in range(1,len(l)):
+    s=s|l[i]
+print(s)
