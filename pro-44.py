c6934943c78efb900282d335654acf7209db6bb7
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-04-01 10:16:59 +0100

    Create pro-44.py

---
+++ pro-44.py
@@ -0,0 +1,12 @@
+#swa
+n,p,q,r=map(int,input().split())
+a=list(map(int,input().split()))
+m=min(a)
+for i in range(0,len(a)):
+  for j in range(i,len(a)):
+    for k in range(j,len(a)):
+      val=p*a[i]+q*a[j]+r*a[k]
+      if val>m:
+        m=val
+print(m)
+© 2019 GitHub, Inc.
