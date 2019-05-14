fea31570b92405c84bff07752f96408283c13164
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-14 12:06:12 +0530

    Update hun_55.py

--- hun_55.py
+++ hun_55.py
@@ -1,10 +1,9 @@
-#swa
-n,k=map(int,input().split(" "))
+#Swa
+n,k=map(int,input().split())
 l=[int(i) for i in input().split()]
 i=0
-while n>0:
-    l.append(l[i])
-    del(l[i])
-    k=k-1
-    break
+while k>0:
+  l.append(l[i])
+  del(l[i])
+  k=k-1
 print(*l)
