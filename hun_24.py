77282dd436882979ea67b377eacd743a100b1f7d
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-14 11:01:48 +0530

    Create hun_24.py

---
+++ hun_24.py
@@ -0,0 +1,13 @@
+#swa
+n,k=map(int,input().split(" "))
+l=list(map(int,input().split(" ")))
+c=0
+for i in range(0,n):
+    for j in range(i+1,n):
+        sum1=l[i]+l[j]
+        if sum1==k:
+            c=c+1
+if c>=1:
+    print("YES")
+else:
+    print("NO")
