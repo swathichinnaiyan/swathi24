d0bcb85000cb07906222e392ef35d55afc08c8a1
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-14 11:06:44 +0530

    Update hun_24.py

--- hun_24.py
+++ hun_24.py
@@ -1,6 +1,6 @@
 #swa
-n,k=map(int,input().split(" "))
-l=list(map(int,input().split(" ")))
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
 c=0
 for i in range(0,n):
     for j in range(i+1,n):
