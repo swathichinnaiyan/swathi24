3e70efc72e92ee0b9c0ba8d99e2411365d701947
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-15 15:20:17 +0530

    Create pro1.py

---
+++ pro1.py
@@ -0,0 +1,14 @@
+#swa
+s=input()
+s=int(s)
+l=[]
+for i in range(0,s):
+    s1=input()
+    l.append(s1)
+c=[]
+for i in zip(*l):
+    if i.count(i[0])==len(i):
+        c.append(i[0])
+    else:
+        break
+print(''.join(c)
