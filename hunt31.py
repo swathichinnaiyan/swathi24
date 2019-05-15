bb3a0df306f0c7c0f8c7ff4e06649d148ab8d48b
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-15 14:44:06 +0530

    Update hunt31.py

--- hunt31.py
+++ hunt31.py
@@ -1,7 +1,15 @@
 #swa
 n=int(input())
-l=[int(x) for x in input().split()]
-a=1
-for i in range(0,len(l)):
-	a=a*l[i]
-print(a)
+l=[int(i) for i in input().split()]
+a=[]
+for i in range(0,n):
+    c=1
+    for j in range(i,n):
+        c=c*l[j]
+    a.append(c)
+for i in range(0,n):
+    c=1
+    for j in range(i+1):
+        c=c*l[j]
+    a.append(c)
+print(max(a))
