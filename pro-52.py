b11a56b23bb5c10214c45f0cc94da1d6ae2e47db
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-15 11:51:45 +0530

    Create pro-52.py

---
+++ pro-52.py
@@ -0,0 +1,13 @@
+#swa
+a,b=map(int,input().split())
+c,d=map(int,input().split())
+e,f=map(int,input().split())
+g,h=map(int,input().split())
+i=d-b
+j=f-h
+k=e-c
+l=g-a
+if i==j== k==l:
+	print("yes")
+else:
+	print("no")
