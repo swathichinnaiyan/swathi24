ced85b5d2a5ee32befe5017df4e910957d574864
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-18 13:56:35 +0100

    Update pr062.py

--- pr062.py
+++ pr062.py
@@ -1,10 +1,14 @@
-#Swa
-n=input()
-n1=list(n)
-c=0
-
-for i in n1:
-	if n1.count(i)==1:
-		c=c+1
-print(c)		
-		
+#pali
+s=input()
+subst=""
+f=0
+op=[]
+if s==s[::-1]:
+  op.append(0)
+for i in range(0,len(s)-1):
+  for j in range(i+1,len(s)):
+    subst=s[i:j+1]
+    if subst==subst[::-1]:
+      op.append(len(s)-len(subst))
+     
+print(min(op))
