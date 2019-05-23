bdd0d8d8785a79776a1b9e3a162175064641a103
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-23 14:31:28 +0100

    Update play129.py

--- play129.py
+++ play129.py
@@ -1,8 +1,9 @@
 #s
-n = int(input(""))
-a = list(map(int,input().split()))
-c = 0
-for i in a:
-    if(i<0):
-        c = c+i
-print(c)
+n=int(input())
+l=list(map(int,input().split()))
+s=[0]
+for i in range(n):
+	for j in range(i,n):
+		c=l[i:j+1]
+		s.append(sum(c))
+print(min(s))
