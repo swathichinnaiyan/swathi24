cff349bc7f65b6ef323abcf1357e2107430fda64
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-23 11:59:58 +0100

    Update hun72.py

--- hun72.py
+++ hun72.py
@@ -1,9 +1,13 @@
 #swa
-a=input().split()
-s=[]
-for i in range(0,len(a)):
-    if i%2==0:
-        s.append(a[i][::-1])
-    else:
-        s.append(a[i])
-print(" ".join(s))        
+s=input()
+if s[-1]==".":
+	s=s[:-1]
+l=list(s.split(" "))
+a=[]
+for i in range(len(l)):
+	if i%2==0:
+		c=l[i]
+		a.append(c[::-1])
+	else:
+		a.append(l[i])
+print(" ".join(a))
