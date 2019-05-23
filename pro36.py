7400cacdb032e331caeab8846d4aa7154984197a
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-23 12:15:39 +0100

    Update pro36.py

--- pro36.py
+++ pro36.py
@@ -2,9 +2,9 @@
 n = int(input())
 a = list(map(int,input().split()))
 c = 0
-for i in range(0,n):
-  for j in range(i+1,n):
-    for k in range(j+1,n):
+for i in range(0,len(a)-2):
+  for j in range(i+1,len(a)-1):
+    for k in range(j+1,len(a)):
       if i<j<k and a[i]>a[j]>a[k]:
         c = c+1
 print(c)
