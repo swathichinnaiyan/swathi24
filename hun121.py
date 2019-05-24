db790c27a463f511e6b96ea88a0c665ca4f50a78
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-24 01:02:04 -0700

    Update hun121.py

--- hun121.py
+++ hun121.py
@@ -1,10 +1,14 @@
-#s
+#pal
 n=input()
+l=n[::-1]
 s=""
-for i in range(len(n)-1):
-  for j in range(i+1,len(n)+1):
-    r=n[i:j+1]
+if n==l:
+    print(n)
+else:
+    for i in range(len(n)-1):
+        for j in range(i+1,len(n)+1):
+            r=n[i:j+1]
     
-    if len(r)>len(s) and r==r[::-1]:
+        if len(r)>len(s) and r==r[::-1]:
             s=r
 print(s)
