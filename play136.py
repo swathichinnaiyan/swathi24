362283a3f53e812e83e6d70983ce5c17c26a97f3
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-25 07:01:54 +0100

    Create play136.py

---
+++ play136.py
@@ -0,0 +1,13 @@
+#s
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+c=0
+for i in range(0,len(l)):
+    if l[i]==k:
+        c=c+1
+if c>=1:
+    print("yes",c)
+else:
+    print("no")
+        
+   
