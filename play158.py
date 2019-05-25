eab584170b5864c5954912f8e4e3c72e8511b786
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-25 10:23:12 +0100

    Update play158.py

--- play158.py
+++ play158.py
@@ -1,10 +1,13 @@
 #s
 m,n=map(int,input().split())
 p=m*n
-c=bin(p)
-
+c=bin(p)[2:]
+a=0
 #print(c)
-for i in c:
-	if i=="1":
-		print(c.index(i)+2)
-		break
+for i in range(len(c)):
+	if c[i]=="1":
+	    a=a+1
+	if a==2:
+	    print(i+1)
+	    break
+		
