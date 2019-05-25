7908d017d7a1dc927d045ac2a5af720e2673bd71
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-25 15:42:32 +0100

    Create play130.py

---
+++ play130.py
@@ -0,0 +1,11 @@
+#s
+n=int(input())
+l=[int(x) for x in input().split()]
+s=[]
+for i in range(n):
+    c=l[:i+1]
+    if sum(c)%2==0:
+        s.append(str(sum(c)))
+    else:
+        s.append(str(l[i]))
+print(" ".join(s))
