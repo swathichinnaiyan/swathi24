53e5f9cd49b4fa8898aec89ac0828cfdc8fbacea
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-27 11:53:21 +0100

    Create play23.py

---
+++ play23.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+input()
+l=list(map(int,input().split()))
+l1=list(map(int,input().split()))
+s=[]
+for i in range(len(l1)):
+	l.append(l1[i])
+	x=max(l)
+	s.append(x)
+print(*s)
