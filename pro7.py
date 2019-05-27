d5b0ba5c0967a1e825b0651f26c4f90398d36701
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-27 10:49:39 +0100

    Create pro7.py

---
+++ pro7.py
@@ -0,0 +1,12 @@
+import math
+n=int(input())
+l=math.log10(n)/math.log10(2)
+#print(l)
+#print(math.ceil(l))
+#print(math.floor(l))
+if math.ceil(l)==math.floor(l):
+	print(0)
+else:
+	a=(n-1)//2
+
+	print(n-b)
