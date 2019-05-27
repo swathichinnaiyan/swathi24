6cd1f2c18d67cb8870c0a028e85727b00d015141
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-27 11:35:27 +0100

    Create play203.py

---
+++ play203.py
@@ -0,0 +1,14 @@
+l=["a","e","i","o","u","A","E","I","O","U"]
+n=int(input())
+c=0
+m=[]
+for i in range(n):
+	a=input()
+	for i in a:
+		if i in l:
+			c+=1
+	m.append([a,c])
+	c=0
+m.sort(key=lambda x:x[1],reverse=True)	
+for i in range(n):
+    print(m[i][0])
