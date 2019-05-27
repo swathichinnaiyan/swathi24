8bdf688ff3b6c5c202c2661eaf70cf03bd04b912
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-27 10:51:57 +0100

    Update pro7.py

--- pro7.py
+++ pro7.py
@@ -2,11 +2,14 @@ import math
 n=int(input())
 l=math.log10(n)/math.log10(2)
 #print(l)
+
 #print(math.ceil(l))
 #print(math.floor(l))
 if math.ceil(l)==math.floor(l):
 	print(0)
 else:
 	a=(n-1)//2
-
+	#print(a)
+	b=a*2
+	#print(b)
 	print(n-b)
