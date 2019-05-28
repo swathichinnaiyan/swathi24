f73685cdf860c38e1548e16c18a1ed9c1289d7ca
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-28 11:18:41 +0100

    Create pro10.py

---
+++ pro10.py
@@ -0,0 +1,10 @@
+
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in range(0,n):
+    for j in range(0,i):
+        if l[j]<l[i]:
+            c=c+l[j]
+print(c)            
+            
