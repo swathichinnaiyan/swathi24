6d2bfebaa8f5203b914787199b32e6fffb6da576
swathichinnaiyan <46440497+swathichinnaiyan@users.noreply.github.com>
2019-05-28 13:22:26 +0100

    Update play66.py

--- play66.py
+++ play66.py
@@ -1,7 +1,8 @@
-a,k=map(int,input().split())
-l=list(map(int,input().split()))
+n=list(map(int,input().split()))
+k=n[len(n)-1]
+l=[i for i in input().split()]
 for i in l:
     if l.count(i)==k:
         print(i)
         break
-    
+#    
